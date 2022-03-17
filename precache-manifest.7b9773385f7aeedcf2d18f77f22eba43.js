ad3a373869b727cfaeb3335cd7aa9d1d35585c10
hrhari1010 <101563443+hrhari1010@users.noreply.github.com>
2022-03-17 16:29:01 +0530

    Add files via upload

---
+++ precache-manifest.7b9773385f7aeedcf2d18f77f22eba43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf39cb2db93a12fcce3ccd7030d4f4f6",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "1d24011e0f0a5d065591",
+    "url": "/calculator/static/css/2.e49b6726.chunk.css"
+  },
+  {
+    "revision": "e39c06b95be0349bb9bd",
+    "url": "/calculator/static/css/main.0de35a7f.chunk.css"
+  },
+  {
+    "revision": "1d24011e0f0a5d065591",
+    "url": "/calculator/static/js/2.92270733.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/calculator/static/js/2.92270733.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e39c06b95be0349bb9bd",
+    "url": "/calculator/static/js/main.9e2e802a.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
